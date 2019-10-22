55b34d836362d20d668b253b25ede29ea91d88ed
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-22 18:56:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,14 +8,9 @@ class Anagram
   end
 
   def match(array)
-    binding.pry
+    # binding.pry
     string_array = []
     string_array << array.each == word.split("").sort
     return string_array
-    # if string_array == word.split("").sort
-    #   return
-    # else
-    #   return
-    # end
   end
 end
