cad71f4b7e25d6313c67d0540e82d9a06c67aee1
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-22 19:10:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
 
   def match(array)
     # binding.pry
-    string_array = array.select == word.split("").sort
-    return string_array
+    array.select == word.split("").sort
+    return 
   end
 end
